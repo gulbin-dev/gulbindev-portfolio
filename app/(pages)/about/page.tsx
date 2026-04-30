8b651a0e420bf7fec773b334fbc56601d3f80d45
gulbin-dev <gulbindev@gmail.com>
2026-04-30 11:16:12 +0800

    refractor: adjust animation on elements

--- app/(pages)/about/page.tsx
+++ app/(pages)/about/page.tsx
@@ -277,7 +277,7 @@ export default function About() {
                 "-=0.3",
               )
               .to(".divider", {
-                delay: 0.5,
+                delay: 0.3,
                 rotate: -65,
                 scaleX: 0.3,
               })
@@ -458,7 +458,7 @@ export default function About() {
                 </span>
               </span>{" "}
               {/*  SVG paper plane */}
-              <div className="hidden desktop:block absolute left-92 top-25 min-w-20 min-h-30">
+              <div className="hidden desktop:block absolute left-180 top-25 min-w-20 min-h-30">
                 <svg className="w-full h-full scale-65 -rotate-45 desktop:scale-100 desktop:rotate-0">
                   <path
                     id="path"
