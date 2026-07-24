9d273e95609aa8efad309e1d4802bb48dbac6fb8
gulbin-dev <gulbindev@gmail.com>
2026-07-24 10:12:34 +0800

    style: improve UI display

--- app/(pages)/(pages)/projects/_components/Projects.tsx
+++ app/(pages)/(pages)/projects/_components/Projects.tsx
@@ -20,15 +20,15 @@ export default function Projects({
   return (
     <ul
       aria-label="Project"
-      className="flex max-w-110 flex-col items-center justify-center gap-2 py-6 tablet:flex-row tablet:items-stretch desktop:gap-4"
+      className="grid grid-cols-1 gap-4 py-6 tablet:grid-cols-2 desktop:gap-6"
     >
       {projectList.projects.map((item) => (
         <li
           role="presentation"
           key={item.id}
-          className="flex tablet:max-w-[50%]"
+          className="w-full flex" 
         >
-          <Card className="grid grid-rows-[min-content_auto_1fr_auto]">
+          <Card className="w-full grid grid-rows-[min-content_auto_1fr_auto]">
             <PreviewVideo folder={item.name} />
 
             <h2 className="relative z-3 row-start-2 py-2 text-size-md">
@@ -41,8 +41,8 @@ export default function Projects({
             >
               <ul aria-label="navigation" className="flex gap-1">
                 <li>
-                  <CTALinkButton link={item.homepage as string} target="_blank">
-                    Visit Website
+                  <CTALinkButton link={item.homepage as string} target="_blank" className={`${item.name === 'gulbindev-portfolio' ? 'bg-this-website': ''}`}>
+                   {item.name === 'gulbindev-portfolio'?  "This Website": "Visit Website" }
                   </CTALinkButton>
                 </li>
                 <li>
