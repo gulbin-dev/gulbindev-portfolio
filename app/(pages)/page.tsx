e225d0c70ddeec708d02373d3c9677d9f4200ed2
gulbin-dev <gulbindev@gmail.com>
2026-09-06 19:10:37 +0800

    style: Remove redundant GridBackground components

--- app/(pages)/page.tsx
+++ app/(pages)/page.tsx
@@ -22,9 +22,8 @@ export default async function HomePage() {
   return (
     <>
       {/* Hero section */}
-      <GridBackground className="hidden desktop:block" />
+      <GridBackground />
       <Section ariaLabel="Hero section" className="desktop:h-75">
-        <GridBackground className="block desktop:hidden" />
         <header className="relative z-1 flex flex-col tablet:col-start-1 tablet:col-end-5 tablet:row-span-3 tablet:row-start-1 desktop:col-start-1 desktop:col-end-7 desktop:row-span-2 desktop:row-start-1">
           <span className="relative mt-6 ml-0 inline-flex w-fit items-center gap-2 rounded-t-2xl pl-2.5 text-foreground-white desktop:ml-4">
             <span className="relative flex h-3.5 w-3.5 items-center justify-center">
